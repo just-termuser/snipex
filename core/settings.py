04c3ccbb460075775a1f9f1cf7c46b74bce068f5
just-termuser <just_anotheruser@tutamail.com>
2026-05-19 07:53:02 -0700

    med

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",
+    "unfold.contrib.simple_history",
+    "unfold.contrib.location_field",
+    "unfold.contrib.constance",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- snippets/admin.py
+++ snippets/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-from .models import Category, Snippet
+from .models import Category, Language, Snippet, Tag
 
-admin.site.register(Category)
-admin.site.register(Snippet)
+
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Snippet)
+class SnippetAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(ModelAdmin):
+    pass
+
+
 # Register your models here.
