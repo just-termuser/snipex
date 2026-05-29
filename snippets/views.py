005eb7cfe18756438afe60ef446e28096a015dc4
just-termuser <just_anotheruser@tutamail.com>
2026-05-29 07:01:51 -0700

    feat: add htmx support

--- snippets/views.py
+++ snippets/views.py
@@ -3,25 +3,27 @@ from django.http import HttpResponse
 
 from .models import Category, Snippet, Tag
 
-
-# Create your views here.
 def home(request):
     context = {
         "snippets": Snippet.objects.filter(is_public=True),
         "categories": Category.objects.all(),
         "current_category": None,
     }
+    if request.headers.get('HX-Request'):
+        return render(request, "snippets/partials/snippet_board.html", context)
+
     return render(request, "snippets/home.html", context)
 
 
 def categories(request, category_id):
     context = {
-        "snippets": Snippet.objects.filter(
-            is_public=True, category__id=category_id
-        ),
+        "snippets": Snippet.objects.filter(is_public=True, category__id=category_id),
         "categories": Category.objects.all(),
         "current_category": Category.objects.get(id=category_id).name,
     }
+    if request.headers.get('HX-Request'):
+        return render(request, "snippets/partials/snippet_board.html", context)
+
     return render(request, "snippets/home.html", context)
 
 def htmx_test(request):
